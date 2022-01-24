ef94ca3366f44fd9994e480161828702baa80a7c
SameetPathan <sameetpathanrs@gmail.com>
2022-01-24 17:40:43 +0530

    v1

--- static/learn.js
+++ static/learn.js
@@ -4,9 +4,9 @@ const ctx = canvas.getContext("2d");
 
 
 let w = canvas.width = window.innerWidth;
-let h = canvas.height = 1500;
+let h = canvas.height = 1650;
 if (canvas.height < 300) {
-  h = canvas.height = 1500;
+  h = canvas.height = 1650;
 }
 let cols = Math.floor(w / 20) + 1;
 let ypos = Array(cols).fill(0);
